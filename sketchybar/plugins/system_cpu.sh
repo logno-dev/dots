30e207a7d853e578aa90e08e7e85ead56b0f0620
Logan Bunch <loganbunch@gmail.com>
2025-07-31 21:06:34 -0700

    Update dotfiles - 2025-07-31 21:06:34

--- sketchybar/plugins/system_cpu.sh
+++ sketchybar/plugins/system_cpu.sh
@@ -2,4 +2,4 @@
 
 CPU_USAGE=$(top -l 1 | grep "CPU usage" | awk '{print $3}' | sed 's/%//')
 
-sketchybar --set $NAME label="${CPU_USAGE}%"
+sketchybar --set $NAME label="CPU: ${CPU_USAGE}%"
--- sketchybar/plugins/system_ram.sh
+++ sketchybar/plugins/system_ram.sh
@@ -6,8 +6,8 @@ if [ -z "$MEMORY_PRESSURE" ]; then
     MEMORY_USED=$(vm_stat | grep "Pages active\|Pages inactive\|Pages speculative\|Pages wired down" | awk '{sum += $3} END {print int(sum * 4096 / 1024 / 1024)}')
     MEMORY_TOTAL=$(sysctl -n hw.memsize | awk '{print int($1 / 1024 / 1024)}')
     MEMORY_PERCENT=$(echo "scale=0; ($MEMORY_USED * 100) / $MEMORY_TOTAL" | bc)
-    sketchybar --set $NAME label="${MEMORY_PERCENT}%"
+    sketchybar --set $NAME label="RAM: ${MEMORY_PERCENT}%"
 else
     MEMORY_USED=$((100 - MEMORY_PRESSURE))
-    sketchybar --set $NAME label="${MEMORY_USED}%"
+    sketchybar --set $NAME label="RAM: ${MEMORY_USED}%"
 fi
